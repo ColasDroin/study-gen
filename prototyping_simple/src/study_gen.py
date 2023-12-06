a89fc80908e6e1345cf8ed912a4729fab4cb293b
Colas <colasdroin@gmail.com>
2023-12-06 12:15:59 +0100

    Refactoring to externalize merge functions

--- prototyping_simple/src/study_gen.py
+++ prototyping_simple/src/study_gen.py
@@ -1,25 +1,3 @@
 from collections import OrderedDict
-
 from .block import Block
 
-
-def compose_blocks(*blocks: Block, output: OrderedDict[str, type]) -> Block:
-    """Compose blocks into a single block
-
-    Args:
-        *blocks (Block): Blocks to compose
-
-    Returns:
-        Block: Composed block
-    """
-    # Create a new block
-    composed_block = Block()
-
-    # Add all blocks to the composed block
-    for block in blocks:
-        composed_block.merge_block(
-            block, "test_merge_function", "This is a test for a merge function", output
-        )
-
-    # Return the composed block
-    return composed_block
