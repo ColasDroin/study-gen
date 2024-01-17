5b40a47ac71b4db497c3a830eb19dcb819521d00
Colas <colasdroin@gmail.com>
2024-01-17 16:35:48 +0100

    Update with mad collider build block

---
+++ prototyping/collider_from_mad/blocks/build_collider_TODO.py
@@ -0,0 +1,87 @@
+# ==================================================================================================
+# --- Imports ---
+# Some imports are optional as they're also declared in dict_imports, but it's
+# helpful to declare them here for linting.
+# ==================================================================================================
+# Standard library imports
+from collections import OrderedDict
+
+# Third party imports
+import xmask as xm
+from cpymad.madx import Madx
+
+# Local imports
+from study_gen.block import Block
+
+# Imports needed for block to work (not detected by linting tools)
+dict_imports = {"xm": "import xmask as xm", "Madx": "from cpymad.madx import Madx"}
+for module, import_statement in dict_imports.items():
+    exec(import_statement)
+
+
+# ==================================================================================================
+# --- Block function ---
+# ==================================================================================================
+# ! Type parameters
+def build_collider_function(links, sanity_checks=True):
+    # Make mad environment
+    xm.make_mad_environment(links=config_mad["links"])
+
+    # Start mad for all beams
+    mad_b1b2 = Madx(command_log="mad_b1b2.log")
+    mad_b4 = Madx(command_log="mad_b4.log")
+
+    # Build sequences
+    ost.build_sequence(mad_b1b2, mylhcbeam=1)
+    ost.build_sequence(mad_b4, mylhcbeam=4)
+
+    # Apply optics (only for b1b2, b4 will be generated from b1b2)
+    ost.apply_optics(mad_b1b2, optics_file=config_mad["optics_file"])
+
+    if sanity_checks:
+        mad_b1b2.use(sequence="lhcb1")
+        mad_b1b2.twiss()
+        ost.check_madx_lattices(mad_b1b2)
+        mad_b1b2.use(sequence="lhcb2")
+        mad_b1b2.twiss()
+        ost.check_madx_lattices(mad_b1b2)
+
+    # Apply optics (only for b4, just for check)
+    ost.apply_optics(mad_b4, optics_file=config_mad["optics_file"])
+    if sanity_checks:
+        mad_b4.use(sequence="lhcb2")
+        mad_b4.twiss()
+        ost.check_madx_lattices(mad_b1b2)
+
+    # Build xsuite collider
+    collider = xlhc.build_xsuite_collider(
+        sequence_b1=mad_b1b2.sequence.lhcb1,
+        sequence_b2=mad_b1b2.sequence.lhcb2,
+        sequence_b4=mad_b4.sequence.lhcb2,
+        beam_config=config_mad["beam_config"],
+        enable_imperfections=config_mad["enable_imperfections"],
+        enable_knob_synthesis=config_mad["enable_knob_synthesis"],
+        rename_coupling_knobs=config_mad["rename_coupling_knobs"],
+        pars_for_imperfections=config_mad["pars_for_imperfections"],
+        ver_lhc_run=config_mad["ver_lhc_run"],
+        ver_hllhc_optics=config_mad["ver_hllhc_optics"],
+    )
+    collider.build_trackers(_context=context)
+
+    if sanity_checks:
+        collider["lhcb1"].twiss(method="4d")
+        collider["lhcb2"].twiss(method="4d")
+    # Return collider
+    return collider
+
+
+# ==================================================================================================
+# --- Block object ---
+# ==================================================================================================
+
+build_distribution = Block(
+    "build_distribution",
+    build_distribution_function,
+    dict_imports=dict_imports,
+    dict_output=OrderedDict([("output_build_distribution", float)]),
+)
---
+++ prototyping/collider_from_mad/blocks/build_initial_hllhc_sequence.py
@@ -0,0 +1,61 @@
+# ==================================================================================================
+# --- Imports ---
+# Some imports are optional as they're also declared in dict_imports, but it's
+# helpful to declare them here for linting.
+# ==================================================================================================
+# Standard library imports
+from collections import OrderedDict
+
+# Third party imports
+from cpymad.madx import Madx
+
+# Local imports
+from study_gen.block import Block
+
+# Imports needed for block to work (not detected by linting tools)
+dict_imports = {"Madx": "from cpymad.madx import Madx"}
+for module, import_statement in dict_imports.items():
+    exec(import_statement)
+
+
+# ==================================================================================================
+# --- Block function ---
+# ==================================================================================================
+def build_initial_hllhc_sequence_function(
+    mad: Madx,
+    beam: str,
+) -> Madx:
+
+    # Select beam
+    mad.input(f"mylhcbeam = {beam}")
+
+    # Build sequence
+    mad.input("""
+    ! Build sequence
+    option, -echo,-warn,-info;
+    if (mylhcbeam==4){
+        call,file="acc-models-lhc/lhcb4.seq";
+    }
+    else {
+        call,file="acc-models-lhc/lhc.seq";
+    };
+    !Install HL-LHC
+    call, file="acc-models-lhc/hllhc_sequence.madx";
+    ! Get the toolkit
+    call,file="acc-models-lhc/toolkit/macro.madx";
+    option, -echo, warn,-info;
+    """)
+
+    return mad
+
+
+# ==================================================================================================
+# --- Block object ---
+# ==================================================================================================
+
+build_initial_hllhc_sequence = Block(
+    "build_initial_hllhc_sequence",
+    build_initial_hllhc_sequence_function,
+    dict_imports=dict_imports,
+    dict_output=OrderedDict([("output_build_initial_hllhc_sequence", Madx)]),
+)
