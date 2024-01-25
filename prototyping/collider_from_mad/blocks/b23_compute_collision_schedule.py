7b2f4482d8a714290621f6187f37473083b0c922
Colas Droin <colasdroin@gmail.com>
2024-01-25 12:08:48 +0100

    Added few more blocks

---
+++ prototyping/collider_from_mad/blocks/b23_compute_collision_schedule.py
@@ -0,0 +1,44 @@
+# ==================================================================================================
+# --- Imports ---
+# Some imports are optional as they're also declared in dict_imports, but it's
+# helpful to declare them here for linting.
+# ==================================================================================================
+# Standard library imports
+from collections import OrderedDict
+
+# Third party imports
+import numpy as np
+
+# Local imports
+from study_gen.block import Block
+
+# Imports needed for block to work (not detected by linting tools)
+dict_imports = {"np": "import numpy as np"}
+
+
+# ==================================================================================================
+# --- Block function ---
+# ==================================================================================================
+def compute_collision_schedule_function(
+    array_b1: np.ndarray, array_b2: np.ndarray
+) -> tuple[int, int, int]:
+
+    # Assert that the arrays have the required length, and do the convolution
+    assert len(array_b1) == len(array_b2) == 3564
+    n_collisions_ip1_and_5 = int(array_b1 @ array_b2)
+    n_collisions_ip2 = int(np.roll(array_b1, 891) @ array_b2)
+    n_collisions_ip8 = int(np.roll(array_b1, 2670) @ array_b2)
+
+    return n_collisions_ip1_and_5, n_collisions_ip2, n_collisions_ip8
+
+
+# ==================================================================================================
+# --- Block object ---
+# ==================================================================================================
+
+compute_collision_schedule = Block(
+    "compute_collision_schedule",
+    compute_collision_schedule_function,
+    dict_imports=dict_imports,
+    dict_output=OrderedDict([("output_compute_collision_schedule", tuple[int, int, int])]),
+)
